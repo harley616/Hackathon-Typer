66ce745886094f40e1c329f5afc591efd65ba9cc
Harley Ewert <harleyewert@cu-biot-8-10.203.158.146.int.colorado.edu>
2024-03-02 11:57:08 -0700

    adding structure

---
+++ src/app/login/page.js
@@ -0,0 +1,19 @@
+
+export default function Login(){
+    return (
+        <div>
+        <h1>Login</h1>
+        <form>
+            <label>
+            Email:
+            <input type="email" />
+            </label>
+            <label>
+            Password:
+            <input type="password" />
+            </label>
+            <button type="submit">Login</button>
+        </form>
+        </div>
+    );
+}
