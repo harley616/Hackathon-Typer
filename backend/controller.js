43f27a7a000a30f0c4a053bf58db0f2070262b5b
Harley Ewert <harleyewert@cu-biot-8-10.203.158.146.int.colorado.edu>
2024-03-02 16:50:41 -0700

    docker and backend working

--- backend/controller.js
+++ backend/controller.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const db = require('../database');
+const db = require('./database');
 
 const registerUser = async (req, res) => {
   const { username, password } = req.body;
   try {
-    const user = await db.one('INSERT INTO users(username, password) VALUES($1, $2) RETURNING *', [username, password]);
+    const user = await db.one('INSERT INTO users(name) VALUES($1, $2) RETURNING *', [username, password]);
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
+  next();
 }
 
 module.exports = {
---
+++ backend/middleware.js
@@ -0,0 +1,17 @@
+function sendSuccessResponse(req, res) {
+    if (res.locals.data) {
+      // console.log('Data in res.locals', res.locals.data);
+      res.status(200).json(res.locals.data);
+    } else {
+      // Fallback if no data is set in res.locals, but middleware is called
+      res.status(200).json(
+          {
+            status: 'success',
+            message: 'Operation completed successfully.',
+          },
+      );
+    }
+  }
+  
+  module.exports = sendSuccessResponse;
+  
