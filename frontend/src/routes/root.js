9fd74fab977d06ad3aab0ea5c223aa880c590bfe
Harley Ewert <harleyewert@cu-biot-8-10.203.158.146.int.colorado.edu>
2024-03-02 15:53:15 -0700

    should be good now ;

---
+++ frontend/src/routes/root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Outlet} from 'react-router-dom';   
+
+export default function Root() {
+  return (
+    <div>
+      <h1>Root</h1>
+      <Outlet />
+    </div>
+  );
+}
