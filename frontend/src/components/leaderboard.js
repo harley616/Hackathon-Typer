4cc9130b820e69b5f7219e31d597c65d426ba9a2
Harley Ewert <harleyewert@cu-biot-8-10.203.158.146.int.colorado.edu>
2024-03-03 11:33:13 -0700

    Revert "mostly working;"
    This reverts commit fa2a68f3279fea5fc049e9df21284827aeb211f2.

--- frontend/src/components/leaderboard.js
+++ frontend/src/components/leaderboard.js
@@ -1,24 +1,9 @@
-import React, {useState} from 'react';
-import Api from '../services/api';
-import '../styles/startStyle.css';
+import React from 'react';
 
 export default function LeaderBoard(){
-    const api = new Api();
-    const [leaderBoardData] = useState(api.getLeaderBoard());
-
     return (
-        <div className="top-scorers">
-            <h2>Top Scorers</h2>
-            <ul className="top-scorers-list">
-                {
-                Array.isArray(leaderBoardData) ? leaderBoardData.map((data, index) => {
-                    return (
-                        <li key={data.name}>{data.name} - {data.score}</li>
-                    );
-                }) : 
-                    <li>Loading/Empty</li>
-                }
-            </ul>
+        <div>
+            <h1>LeaderBoard</h1>
         </div>
     );
 }
