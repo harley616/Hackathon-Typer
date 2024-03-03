6c68a676d62ee630a28c4e4b5c1d76133fa93417
Harika Kondur <harikakondur3@gmail.com>
2024-03-02 17:04:00 -0700

    finished getPoem function

---
+++ frontend/src/services/getPoem.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+const axios = require('axios');
+
+const levels={
+    easy:"5/lines",
+    medium:"10/lines",
+    hard:"15/lines"
+}
+
+export default function getPoem(level){
+    const path=levels[level];
+
+    axios.get(`https://poetrydb.org/random,linecount/1;${path}`)
+    .then(response => {
+      console.log(response.data[0].lines);
+      return response.data[0].lines;
+    })
+    .catch(error => {
+      console.error('error!', error);
+      return false;
+    });
+}
